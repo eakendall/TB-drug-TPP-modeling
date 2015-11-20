b55fcc3bb588d69afea3d055b62d5e72ae5eaf8e
eakendall <emily.kendall@gmail.com>
2015-11-20 10:11:31 -0500

    minor fixes to makemat function

--- TPPmat.R
+++ TPPmat.R
@@ -1,3 +1,5 @@
+library('deSolve')
+
 # set up compartments
 setup.model <- function()
 {
@@ -21,19 +23,25 @@ pars <- function(setup)
   p$selfcurerate <- 0.2; p$relapserate <- 1
   p$dxrate <- c(1,2); p$DSTrif <- c(0.1,1); names(p$dxrate) <- names(p$DSTrif) <- c("An","Ap") #by new or previously treated
   p$availability <- 1 # MAY NEED TO MAKE THIS TIME-DEPENDENT FOR SCALE-UP
-  p$DSTnew <- c(0,1) #companion drugs, novel drug (doesn't depend on rif or retreatment status)
-  p$eligibility <- c(1,0.9) #by HIV status
-  p$targetpop <- c(1,1) #will change for MDR=(0,1), DS=(1,0), or panTB=(1,1)
-  p$fail_s <- c(0.02,0.5); p$fail_s <- rep(p$fail_s, each=4) #fraction ineffective on SOC DS regimen, by rif resistance
-  p$fail_r <- 0.12; p$fail_r <- rep(p$fail_r, 8) #fraction ineffective on SOC MDR regimen
-  p$fail_n <- c(p$fail_s[1:3] + c(0, 0.08, 0.5), 1); p$fail_n <- rep(p$fail_n, times=2)
-  p$acqres_s <- 0.005 #fraction acquiring rif resistance on SOC DS regimen
-  # currently no acqres with regimen r
-  p$acqres_n <- t(array(c( 0, 0.1, 0.05, 0.005,
-                          0, 0, 0.4, 0.04, 
-                          0, 0, 0, 0.1, 
-                          0, 0, 0, 0), dim=c(4,4)))
-
+  p$DSTnew <- c(0,1); names(p$DSTnew) <- c("c","n") #companion drugs, novel drug (doesn't depend on rif or retreatment status)
+  p$eligibility <- c(1,0.9); names(p$eligibility) <- setup$Hnames #by HIV status
+  p$ltfurate <- c(0.01, 0.01, 0.01); names(p$ltfurate) <- setup$regimens
+  p$targetpop <- c(1,1); names(p$targetpop) <- c("DS", "MDR") #will change for MDR=(0,1), DS=(1,0), or panTB=(1,1)
+  
+  fail_s <- c(0.02,0.5); fail_s <- rep(fail_s, each=4) #fraction ineffective on SOC DS regimen, by resistance
+  fail_r <- 0.12; fail_r <- rep(fail_r, 8) #fraction ineffective on SOC MDR regimen
+  fail_n <- c(fail_s[1:3] + c(0, 0.08, 0.5), 1); fail_n <- rep(fail_n, times=2)
+  p$failmat <- rbind(fail_s, fail_r, fail_n); dimnames(p$failmat)= list(setup$regimens, setup$Rnames)
+  
+  p$acqresmat <- array(0,dim=c(8,8,3)); dimnames(p$acqresmat)=list(setup$Rnames, setup$Rnames, setup$regimens) # from old resistance (down) to new resistance (across), by regimen 
+  acqres_s <- 0.005; p$acqresmat[,,"s"] <- rbind(cbind(0,0,0,0, acqres_s * diag(4)), array(0, dim=c(4,8)))  
+  p$acqresmat[,,"r"] <- array(0,dim=c(8,8)) # currently no acqres with regimen r
+  acqres_n <- t(array(c( 0, 0.1, 0.05, 0.005,
+                        0, 0, 0.4, 0.04, 
+                        0, 0, 0, 0.1, 
+                        0, 0, 0, 0), dim=c(4,4))); dimnames(acqres_n) <- list("R1"=c("0","c","n","cn"), "R2"=c("0","c","n","cn"))
+  p$acqresmat[,,"n"] <- rbind(cbind(acqres_n,0,0,0,0), cbind(0,0,0,0, acqres_n))
+  
   p$relapse_optimal <- c(0.04,0.12,0.02); names(p$relapse_optimal) <- setup$regimens #for fully susceptible and treatment completed
   p$relapse_resistance <- array(c( rep(p$relapse_optimal['s'],4), rep(0.6, 4), #translates 50% failure, 30% relapse, 20% cure into probabily of non-failures who relaspe
                                  rep(p$relapse_optimal['r'],8),  #again assuming resistance doesn't affect outcomes of SOC MDR regimen
@@ -43,14 +51,19 @@ pars <- function(setup)
   p$durations <- c(6, 18, 4); names(p$durations) <- setup$regimens #in months, by regimen
   p$relapse246 <- c(0.3, 0.12, 0); names(p$relapse246) <- c("2mo","4mo","6mo") #extra relapse added by thirds of course completed (will interpolate between these)
   
+  p <- c(p,setup)
+  
   return(p)
 }
 
-  
+
+
 # make matrix of non-dynamic state transitions (transitions are from first to second state, and diagonal is used for mortality)
-makemat <- function(setup, pars)
+makemat <- function(pars)
 {
-  mat <- array(0, dim=c(8+Tperiods*length(regimens), 8+Tperiods*length(regimens), 8, 8, 2, 2)); dimnames(mat) <- list("1T"=Tnames, "2T"=Tnames, "1R"=Rnames, "2R"=Rnames, "1H"=Hnames, "2H"=Hnames)
+  with(pars, 
+  {mat <- array(0, dim=c(8+Tperiods*length(regimens), 8+Tperiods*length(regimens), 8, 8, 2, 2)); dimnames(mat) <- list("1T"=Tnames, "2T"=Tnames, "1R"=Rnames, "2R"=Rnames, "1H"=Hnames, "2H"=Hnames)
+  mat
   
   ## HIV infection 
   for (jr in Rnames) for (jt in Tnames) {  mat[jt, jt, jr, jr,"Hn","Hp"] <- mat[jt, jt, jr, jr,"Hn","Hp"] + hivrate }
@@ -84,15 +97,8 @@ makemat <- function(setup, pars)
     ) 
     startmat[,1:4,"s"] <- 1 - startmat[,1:4,"n"]; startmat[,5:8,"s"] <- (1-DSTrif) * (1 - startmat[,5:8,"n"]) 
     startmat[,5:8,"r"] <- DSTrif * (1 - startmat[,5:8,"n"])
-        
-    # some transition to failure as opposed to initial treatment response: 
-    failmat <- rbind(fail_s, fail_r, fail_n); dimnames(failmat)= list(regimens, Rnames)
     
-    # then create an array for the probabilities of acquiring resistance when treatment starts (and going immediately to pending relapse for the new strain):
-    acqresmat <- array(0,dim=c(8,8,3)); dimnames(acqresmat)=list(Rnames, Rnames, regimens) # from old resistance (down) to new resistance (across), by regimen 
-    acqresmat[,,"n"] <- rbind(cbind(acqres_n,0,0,0,0), cbind(0,0,0,0, acqres_n))
-    acqresmat[,,"s"] <- rbind(cbind(0,0,0,0, acqres_s * diag(4)), array(0, dim=c(4,8)))  
-    acqresmat[,,"r"] <- array(0,dim=c(8,8))
+    
   
     # now assign to ineffective treatment, or pending relapse with acquired resistance, or on first phase of effective treatment:
     for (it in c("An", "Ap")) 
@@ -109,19 +115,20 @@ makemat <- function(setup, pars)
     ## progression through effective treatment to either relapse or cure (or to active if lost to follow up during first 2 months)
     
     # risk of loss to follow up during each time period, and with relapse vs cure depending on fraction of course completed (assuming losses occur in the middle of each time period on average)
-    relapsefracs <- function(d=durations, period, lengths=Tperiod.lengths) # use relapse %s at 2, 4, and 6 months (defined in pars) for a 6 month regimen, and interpolate linearly 2--4 and 4--6 to get relapse % after any fraction of treatment course
+    relapsefracs <- function(period) # use relapse %s at 2, 4, and 6 months (defined in pars) for a 6 month regimen, and interpolate linearly 2--4 and 4--6 to get relapse % after any fraction of treatment course
     {
-      fraction_completed <- (sum(lengths[1:period-1])+1/2*lengths[period])/d
-      relapse <- array(0, dim=c(8,3)); dimnames(relapse) <- list(Rnames, regimens)
-      relapse[,fraction_completed < 1/3] <- 1 
-      relapse[,fraction_completed >= 1/3 & fraction_completed < 2/3] <- t(t(relapse_resistance + relapse246[2]) + (relapse246[1]-relapse246[2])*(2/3-fraction_completed))[, fraction_completed >= 1/3 & fraction_completed < 2/3]
-      relapse[,fraction_completed >= 2/3 & fraction_completed < 1] <- t(t(relapse_resistance + relapse246[3]) + (relapse246[3]-relapse246[3])*(1-fraction_completed))[, fraction_completed >= 2/3 & fraction_completed < 1]
-      relapse[,fraction_completed >= 1] <- relapse_resistance[, fraction_completed >= 1]
-      relapse[relapse>1] <- 1 #if sum exceeds 100%, set to 100% relapse
-      return(relapse)
+        fraction_completed <- (sum(Tperiod.lengths[1:period-1])+1/2*Tperiod.lengths[period])/durations
+        relapse <- array(0, dim=c(8,3)); dimnames(relapse) <- list(Rnames, regimens)
+        relapse[,fraction_completed < 1/3] <- 1 
+        relapse[,fraction_completed >= 1/3 & fraction_completed < 2/3] <- t(t(relapse_resistance + relapse246[2]) + (relapse246[1]-relapse246[2])*(2/3-fraction_completed))[, fraction_completed >= 1/3 & fraction_completed < 2/3]
+        relapse[,fraction_completed >= 2/3 & fraction_completed < 1] <- t(t(relapse_resistance + relapse246[3]) + (relapse246[3]-relapse246[3])*(1-fraction_completed))[, fraction_completed >= 2/3 & fraction_completed < 1]
+        relapse[,fraction_completed >= 1] <- relapse_resistance[, fraction_completed >= 1]
+        relapse[relapse>1] <- 1 #if sum exceeds 100%, set to 100% relapse
+        return(relapse) 
     }
+
     
-    # progression through treatment, assuming 1%/month risk of loss to follow up
+    # progression through treatment, including a monthly rate of loss to follow up
     for (jr in Rnames) for (reg in regimens)
     {
       periods <- paste0("T", reg, 1:Tperiods)
@@ -129,11 +136,11 @@ makemat <- function(setup, pars)
       {
         relapses <- relapsefracs(period=jt)
         if (jt==1) 
-        { mat[periods[1],c(periods[2],"Ap"),jr,jr,jh,jh] <- mat[periods[1],c(periods[2],"Ap"),jr,jr,jh,jh] + (c(0,1) + c(1,-1)*(0.99^Tperiod.lengths[1])) 
+        { mat[periods[1],c(periods[2],"Ap"),jr,jr,jh,jh] <- mat[periods[1],c(periods[2],"Ap"),jr,jr,jh,jh] + (c(0,1) + c(1,-1)*((1-ltfurate[reg])^Tperiod.lengths[1])) 
         } else 
           if (sum(Tperiod.lengths[1:jt]) < durations[reg]) 
         { mat[periods[jt],c(periods[jt+1],"C","R"),jr,jr,jh,jh] <- 
-                mat[periods[jt],c(periods[jt+1],"C","R"),jr,jr,jh,jh] + c( 0.99^Tperiod.lengths[jt], (1-0.99^Tperiod.lengths[jt])*c(1-relapses[jr, reg], relapses[jr, reg])) 
+                mat[periods[jt],c(periods[jt+1],"C","R"),jr,jr,jh,jh] + c( (1-ltfurate[reg])^Tperiod.lengths[jt], (1-(1-ltfurate[reg])^Tperiod.lengths[jt])*c(1-relapses[jr, reg], relapses[jr, reg])) 
         } else 
           if (sum(Tperiod.lengths[1:jt+1]) <= durations[reg] | jt==Tperiods) 
         { mat[periods[jt],c("C","R"),jr,jr,jh,jh] <- 
@@ -144,12 +151,15 @@ makemat <- function(setup, pars)
   
   }
   return(mat)
+  })
 }
+
+
 
 
 # dynamic model transitions
 
-dxdt <- function(setup, pars, tally,...)
+dxdt <- function(t, state, params)
 {
   # infection
   # superinfection
@@ -173,3 +183,6 @@ dxdt <- function(setup, pars, tally,...)
 }
 
 run.model(dxdt, init, pars, ...)
+
+
+params <- pars(); params$mat <- make.mat(params)
